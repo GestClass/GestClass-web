ad8081d4c9cbcd807f6a049b20f170d4308f6713
Otávio Barreto de Oliveira Silva <otaviosilva2632@gmail.com>
2020-03-20 20:47:46 -0300

    Otavio: alteraçao da home, adicionado form de login

--- banco/Banco_de_dados_GestClass.sql
+++ banco/Banco_de_dados_GestClass.sql
@@ -4,51 +4,73 @@ DEFAULT COLLATE UTF8_GENERAL_CI;
 
 USE GestClass;
 
+CREATE TABLE escola (
+	ID_escola INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    nome_escola VARCHAR(70) NOT NULL,
+    logradouro VARCHAR (70) NOT NULL,
+    numero INTEGER NOT NULL,
+    bairro VARCHAR (50) NOT NULL,
+    estado VARCHAR (30) NOT NULL,
+    CNPJ VARCHAR(18) NOT NULL,
+    telefone VARCHAR(13) NOT NULL,
+    email VARCHAR(65) NOT NULL,
+    data_pagamento DATE NOT NULL,
+    quantidade_alunos INTEGER,
+    fk_id_tipos_turma INTEGER NOT NULL 
+);
+
+CREATE TABLE tipo_turmas (
+	ID_tipo_turmas INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    nome_tipo_turma VARCHAR(30) NOT NULL
+);
+
 CREATE TABLE tipo_usuario(
-	nome_usuario VARCHAR(50) NOT NULL,
-	ID_tipo_usuario INTEGER NOT NULL PRIMARY KEY
+	ID_tipo_usuario INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    nome_usuario VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE disciplina (
-	nome_disciplina VARCHAR(50),
-    ID_disciplina INTEGER AUTO_INCREMENT PRIMARY KEY,
+	ID_disciplina INTEGER AUTO_INCREMENT PRIMARY KEY,
+    nome_disciplina VARCHAR(50),    
     fk_id_professor INTEGER   
 );
-
- ALTER TABLE disciplina ADD CONSTRAINT fk_id_professor FOREIGN KEY (fk_id_professor) REFERENCES professor (ID_professor);
 
 CREATE TABLE funcionario (
 	ID_funcionario INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	nome_funcionario VARCHAR (50) NOT NULL,
+    foto VARCHAR(255),
     rg VARCHAR(15) NOT NULL,
     cpf VARCHAR(12) NOT NULL,
-    foto VARCHAR(50) NOT NULL,
-    fk_id_tipo_usuario INTEGER NOT NULL
+	email VARCHAR(65) NOT NULL,
+    senha VARCHAR(50) NOT NULL,
+    telefone VARCHAR(13),
+    celular VARCHAR(14) NOT NULL,
+    fk_id_tipo_usuario INTEGER NOT NULL,
+    fk_dados_professores_id_dados_professor INTEGER NOT NULL,
+    fk_escola_funcionario INTEGER NOT NULL
 );
-
-ALTER TABLE funcionario ADD CONSTRAINT fk_id_tipo_usuario FOREIGN KEY (fk_id_tipo_usuario) REFERENCES tipo_usuario (ID_tipo_usuario);
 
 CREATE TABLE aluno (
 	RA INTEGER PRIMARY KEY,
     foto VARCHAR(255),
     nome_aluno VARCHAR (70) NOT NULL,
     numero_celular VARCHAR (16) NOT NULL,    
-    logradouro VARCHAR (50) NOT NULL,
+    logradouro VARCHAR (70) NOT NULL,
     numero INTEGER NOT NULL,
     bairro VARCHAR (50) NOT NULL,
     estado VARCHAR (30) NOT NULL,
     RG VARCHAR (12) NOT NULL,
     CPF VARCHAR (14) NOT NULL, 
+    email VARCHAR(65) NOT NULL,
+    senha VARCHAR(50) NOT NULL,
+    celular VARCHAR(14),
+    telefone VARCHAR(13),
     data_nascimento DATE NOT NULL,
-    fk_id_responsavel INTEGER,
-    fk_id_dados_alunos INTEGER,
-    fk_usuario_id_tipo_usuario INTEGER
-    
+    fk_id_responsavel INTEGER NOT NULL,
+    fk_id_dados_alunos INTEGER NOT NULL,
+    fk_usuario_id_tipo_usuario INTEGER NOT NULL,
+    fk_escola_aluno INTEGER NOT NULL
 );
-
-ALTER TABLE aluno ADD CONSTRAINT fk_id_responsavel FOREIGN KEY (fk_id_responsavel) REFERENCES responsavel (ID_responsavel);
-ALTER TABLE aluno ADD CONSTRAINT fk_id_dados_alunos FOREIGN KEY (fk_id_dados_alunos) REFERENCES dados_aluno (ID_dados_alunos);
-ALTER TABLE aluno ADD CONSTRAINT fk_usuario_id_tipo_usuario FOREIGN KEY (fk_usuario_id_tipo_usuario) REFERENCES tipo_usuario (ID_tipo_usuario);
 
 CREATE TABLE responsavel (
 	ID_responsavel INTEGER PRIMARY KEY AUTO_INCREMENT,
@@ -61,28 +83,27 @@ CREATE TABLE responsavel (
     estado VARCHAR (30) NOT NULL,
     RG VARCHAR (12) NOT NULL,
     CPF VARCHAR (14) NOT NULL, 
+    email VARCHAR(65) NOT NULL,
+    senha VARCHAR(50) NOT NULL,
+    pin INTEGER(6) NOT NULL,
+    telefone VARCHAR(13),
+    telefone_comercial VARCHAR(13),
+    celular VARCHAR(14) NOT NULL,
     data_nascimento DATE NOT NULL,
-    fk_ra_aluno INTEGER,
-    fk_id_dados_alunos_responsavel INTEGER,
-    fk_usuario_id_tipo_usuario_responsavel INTEGER
-    
+    fk_ra_aluno INTEGER NOT NULL,
+    fk_id_dados_alunos_responsavel INTEGER NOT NULL,
+    fk_usuario_id_tipo_usuario_responsavel INTEGER NOT NULL,
+    fk_escola_responsavel INTEGER NOT NULL
 );
-
-ALTER TABLE responsavel ADD CONSTRAINT fk_ra_aluno FOREIGN KEY (fk_ra_aluno) REFERENCES aluno (RA);
-ALTER TABLE responsavel ADD CONSTRAINT fk_id_dados_alunos_responsavel FOREIGN KEY (fk_id_dados_alunos_responsavel) REFERENCES dados_aluno (ID_dados_alunos);
-ALTER TABLE responsavel ADD CONSTRAINT fk_usuario_id_tipo_usuario_responsavel FOREIGN KEY (fk_usuario_id_tipo_usuario_responsavel) REFERENCES tipo_usuario (ID_tipo_usuario);
 
 CREATE TABLE dados_aluno (
-ID_dados_alunos INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
-notas DECIMAL (2,2) NOT NULL,
-presenca BOOLEAN,
-observacoes VARCHAR (255) ,
-fk_ra_aluno_dados_alunos INTEGER NOT NULL,
-fk_id_disciplina_dados_alunos INTEGER NOT NULL
+    ID_dados_alunos INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    notas DECIMAL (2,2) NOT NULL,
+    presenca BOOLEAN,
+    observacoes VARCHAR (255) ,
+    fk_ra_aluno_dados_alunos INTEGER NOT NULL,
+    fk_id_disciplina_dados_alunos INTEGER NOT NULL
 );
-
-ALTER TABLE dados_aluno ADD CONSTRAINT fk_ra_aluno_dados_alunos FOREIGN KEY (fk_ra_aluno_dados_alunos) REFERENCES aluno (RA);
-ALTER TABLE dados_aluno ADD CONSTRAINT fk_id_disciplina_dados_alunos FOREIGN KEY (fk_id_disciplina_dados_alunos) REFERENCES disciplina (ID_disciplina);
 	
 CREATE TABLE turma (
 	ID_turma INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
@@ -90,28 +111,18 @@ CREATE TABLE turma (
     fk_ra_aluno_turma INTEGER
 );
 
-ALTER TABLE turma ADD CONSTRAINT fk_ra_aluno_turma FOREIGN KEY (fk_ra_aluno_turma) REFERENCES aluno (RA);
-
-
-CREATE TABLE professor (
-	ID_professor INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
+CREATE TABLE dados_professor (
+	ID_dados_professor INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    fk_id_funcionario_professor INTEGER NOT NULL,
     fk_id_disciplina_professor INTEGER, 
-    fk_id_turma_professor INTEGER,
-    fk_tipo_usuario_id_tipo_usuario_professor INTEGER
+    fk_id_turma_professor INTEGER
 );
-
-ALTER TABLE professor ADD CONSTRAINT fk_id_disciplina_professor FOREIGN KEY (fk_id_disciplina_professor) REFERENCES disciplina (ID_disciplina);
-ALTER TABLE professor ADD CONSTRAINT fk_id_turma_professor FOREIGN KEY (fk_id_turma_professor) REFERENCES TURMA (ID_turma);
-ALTER TABLE professor ADD CONSTRAINT fk_tipo_usuario_id_tipo_usuario_professor FOREIGN KEY (fk_tipo_usuario_id_tipo_usuario_professor) REFERENCES tipo_usuario (ID_tipo_usuario);
 
 CREATE TABLE pagamento (
 	id_pagamento INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
     data_pagamento DATE NOT NULL,
 	fk_id_responsavel_pagamento INTEGER NOT NULL
 );
-
-ALTER TABLE pagamento ADD CONSTRAINT fk_id_responsavel_pagamento FOREIGN KEY (fk_id_responsavel_pagamento) REFERENCES responsavel (ID_responsavel);
-
 
 CREATE TABLE contato (
 	ID_mensagem INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
@@ -126,27 +137,93 @@ CREATE TABLE contato (
     fk_recebimento_professor_fk_professor INTEGER
 );
 
+ALTER TABLE dados_professor ADD CONSTRAINT fk_id_funcionario_professor FOREIGN KEY (fk_id_funcionario_professor) REFERENCES funcionario (ID_funcionario);
+
+ALTER TABLE dados_professor ADD CONSTRAINT fk_id_disciplina_professor FOREIGN KEY (fk_id_disciplina_professor) REFERENCES disciplina (ID_disciplina);
+
+ALTER TABLE dados_professor ADD CONSTRAINT fk_id_turma_professor FOREIGN KEY (fk_id_turma_professor) REFERENCES TURMA (ID_turma);
+
+ALTER TABLE pagamento ADD CONSTRAINT fk_id_responsavel_pagamento FOREIGN KEY (fk_id_responsavel_pagamento) REFERENCES responsavel (ID_responsavel);
+
 ALTER TABLE contato ADD CONSTRAINT fk_envio_aluno_ra_aluno FOREIGN KEY (fk_envio_aluno_ra_aluno) REFERENCES aluno (RA);
+
 ALTER TABLE contato ADD CONSTRAINT fk_envio_responsavel_id_responsavel FOREIGN KEY (fk_envio_responsavel_id_responsavel) REFERENCES responsavel (ID_responsavel);
+
 ALTER TABLE contato ADD CONSTRAINT fk_envio_funcionario_id_funcionario FOREIGN KEY (fk_envio_funcionario_id_funcionario) REFERENCES funcionario (ID_funcionario);
-ALTER TABLE contato ADD CONSTRAINT fk_envio_professor_fk_professor FOREIGN KEY (fk_envio_professor_fk_professor) REFERENCES professor (ID_professor);
+
 ALTER TABLE contato ADD CONSTRAINT fk_recebimento_aluno_ra_aluno FOREIGN KEY (fk_recebimento_aluno_ra_aluno) REFERENCES aluno (RA);
+
 ALTER TABLE contato ADD CONSTRAINT fk_recebimento_responsavel_id_responsavel FOREIGN KEY (fk_recebimento_responsavel_id_responsavel) REFERENCES responsavel (ID_responsavel);
+
 ALTER TABLE contato ADD CONSTRAINT fk_recebimento_funcionario_id_funcionario FOREIGN KEY (fk_recebimento_funcionario_id_funcionario) REFERENCES funcionario (ID_funcionario);
-ALTER TABLE contato ADD CONSTRAINT fk_recebimento_professor_fk_professor FOREIGN KEY (fk_recebimento_professor_fk_professor) REFERENCES professor (ID_professor);
+
+ALTER TABLE aluno ADD CONSTRAINT fk_id_responsavel FOREIGN KEY (fk_id_responsavel) REFERENCES responsavel (ID_responsavel);
+
+ALTER TABLE aluno ADD CONSTRAINT fk_id_dados_alunos FOREIGN KEY (fk_id_dados_alunos) REFERENCES dados_aluno (ID_dados_alunos);
+
+ALTER TABLE aluno ADD CONSTRAINT fk_usuario_id_tipo_usuario FOREIGN KEY (fk_usuario_id_tipo_usuario) REFERENCES tipo_usuario (ID_tipo_usuario);
+
+ALTER TABLE aluno ADD CONSTRAINT fk_escola_aluno FOREIGN KEY (fk_escola_aluno) REFERENCES escola (ID_escola);
+
+ALTER TABLE funcionario ADD CONSTRAINT fk_dados_professores_id_dados_professor FOREIGN KEY (fk_dados_professores_id_dados_professor) REFERENCES dados_professor (ID_dados_professor);
+
+ALTER TABLE funcionario ADD CONSTRAINT fk_id_tipo_usuario FOREIGN KEY (fk_id_tipo_usuario) REFERENCES tipo_usuario (ID_tipo_usuario);
+
+ALTER TABLE funcionario ADD CONSTRAINT fk_escola_funcionario FOREIGN KEY (fk_escola_funcionario) REFERENCES escola (ID_escola);
+
+ALTER TABLE escola ADD CONSTRAINT fk_id_tipos_turma FOREIGN KEY (fk_id_tipos_turma) REFERENCES tipo_turmas (ID_tipo_turmas);
+
+ALTER TABLE disciplina ADD CONSTRAINT fk_id_professor FOREIGN KEY (fk_id_professor) REFERENCES funcionario (ID_funcionario);
+
+ALTER TABLE responsavel ADD CONSTRAINT fk_ra_aluno FOREIGN KEY (fk_ra_aluno) REFERENCES aluno (RA);
+
+ALTER TABLE responsavel ADD CONSTRAINT fk_id_dados_alunos_responsavel FOREIGN KEY (fk_id_dados_alunos_responsavel) REFERENCES dados_aluno (ID_dados_alunos);
+
+ALTER TABLE responsavel ADD CONSTRAINT fk_usuario_id_tipo_usuario_responsavel FOREIGN KEY (fk_usuario_id_tipo_usuario_responsavel) REFERENCES tipo_usuario (ID_tipo_usuario);
+
+ALTER TABLE responsavel ADD CONSTRAINT fk_escola_responsavel FOREIGN KEY (fk_escola_responsavel) REFERENCES escola (ID_escola);
+
+ALTER TABLE dados_aluno ADD CONSTRAINT fk_ra_aluno_dados_alunos FOREIGN KEY (fk_ra_aluno_dados_alunos) REFERENCES aluno (RA);
+
+ALTER TABLE dados_aluno ADD CONSTRAINT fk_id_disciplina_dados_alunos FOREIGN KEY (fk_id_disciplina_dados_alunos) REFERENCES disciplina (ID_disciplina);
+
+ALTER TABLE turma ADD CONSTRAINT fk_ra_aluno_turma FOREIGN KEY (fk_ra_aluno_turma) REFERENCES aluno (RA);
 
 CREATE TABLE `events` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `title` varchar(100) CHARACTER SET utf8 NOT NULL,
-  `color` varchar(10) CHARACTER SET utf8 NOT NULL,
-  `start` date NOT NULL,
-  `end` date NOT NULL,
+  `id` INTEGER(11) NOT NULL AUTO_INCREMENT,
+  `title` VARCHAR(100) CHARACTER SET utf8 NOT NULL,
+  `color` VARCHAR(10) CHARACTER SET utf8 NOT NULL,
+  `start` DATETIME NOT NULL,
+  `end` DATETIME NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
 	
-  drop table events;  
     
-/*DROP DATABASE GestClass;*/
+    /*	-	INSERTS IN DESCRIPTIONS TABLES	-	*/
+    
+    
+INSERT INTO tipo_turmas (nome_tipo_turma) VALUES ('berçario');
+INSERT INTO tipo_turmas (nome_tipo_turma) VALUES ('pre-escola');
+INSERT INTO tipo_turmas (nome_tipo_turma) VALUES ('ensino fundamental I');
+INSERT INTO tipo_turmas (nome_tipo_turma) VALUES ('ensino fundamental II');
+INSERT INTO tipo_turmas (nome_tipo_turma) VALUES ('ensino medio');
+
+INSERT INTO tipo_usuario (nome_usuario) VALUES ('diretor');
+INSERT INTO tipo_usuario (nome_usuario) VALUES ('secretaria');
+INSERT INTO tipo_usuario (nome_usuario) VALUES ('professor');
+INSERT INTO tipo_usuario (nome_usuario) VALUES ('responsavel');
+INSERT INTO tipo_usuario (nome_usuario) VALUES ('aluno');
+
+
+/*	-	SELECTS OF DESCRIPTIONS TABLES	-	*/
+
+
+SELECT * FROM tipo_turmas;
+
+SELECT * FROM tipo_usuario;
+
+    
+/*	-	DROP DATABASE GestClass;	-	*/
 
 
 
--- index.php
+++ index.php
@@ -48,9 +48,6 @@
                             <li>
                                 <a href="#planos" class="btn-flat btnTextLight">Planos</a>
                             </li>
-                            <li>
-                                <a href="#" data-izimodal-open="#modalLogin" class="btn-flat btnLight trigger">Login</a>
-                            </li>
                         </ul>
                     </div>
                 </div>
@@ -69,68 +66,47 @@
             <li>
                 <a href="#planos" class="waves-effect waves-light">Planos</a>
             </li>
-            <li>
-                <a href="#" data-izimodal-open="#modalLogin" class="waves-effect waves-light trigger">Login</a>
-            </li>
         </ul>
     </header>
 
-    <div class="slider secao">
-        <ul class="slides">
-            <li>
-                <img src="./assets/img/s.png" class="lax" data-lax-opacity="0 0.3, 500 0" />
-                <div class="caption">
-                    <div class="row grey-text text-darken-4 lax" data-lax-translate-y="0 0, 400 -50">
-                        <div class="col s12 m12 l7">
-                            <h2 class="hide-on-med-and-down">Vamos começar</h2>
-                            <blockquote>
-                                GestClass é uma plataforma que ajuda o corpo docente escolar a gerenciar o ambiene
-                                educacional com mais facilidade
-                            </blockquote>
-                            <a href="contratacao.php" class="btn-flat btn-large btnDark btnHome">
-                                Quero fazer parte
-                            </a>
-                            <a href="#recursos" class="btn-flat btn-large btnDarkFill btnHome">
-                                Veja mais
-                            </a>
-                        </div>
-                        <div class="col l5 center hide-on-med-and-down">
-                            <svg class="machine" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
-                                viewBox="0 0 645 526">
-                                <defs />
-                                <g>
-                                    <path x="-173,694" y="-173,694" class="large-shadow"
-                                        d="M645 194v-21l-29-4c-1-10-3-19-6-28l25-14 -8-19 -28 7c-5-8-10-16-16-24L602 68l-15-15 -23 17c-7-6-15-11-24-16l7-28 -19-8 -14 25c-9-3-18-5-28-6L482 10h-21l-4 29c-10 1-19 3-28 6l-14-25 -19 8 7 28c-8 5-16 10-24 16l-23-17L341 68l17 23c-6 7-11 15-16 24l-28-7 -8 19 25 14c-3 9-5 18-6 28l-29 4v21l29 4c1 10 3 19 6 28l-25 14 8 19 28-7c5 8 10 16 16 24l-17 23 15 15 23-17c7 6 15 11 24 16l-7 28 19 8 14-25c9 3 18 5 28 6l4 29h21l4-29c10-1 19-3 28-6l14 25 19-8 -7-28c8-5 16-10 24-16l23 17 15-15 -17-23c6-7 11-15 16-24l28 7 8-19 -25-14c3-9 5-18 6-28L645 194zM471 294c-61 0-110-49-110-110S411 74 471 74s110 49 110 110S532 294 471 294z" />
-                                </g>
-                                <g>
-                                    <path x="-136,996" y="-136,996" class="medium-shadow"
-                                        d="M402 400v-21l-28-4c-1-10-4-19-7-28l23-17 -11-18L352 323c-6-8-13-14-20-20l11-26 -18-11 -17 23c-9-4-18-6-28-7l-4-28h-21l-4 28c-10 1-19 4-28 7l-17-23 -18 11 11 26c-8 6-14 13-20 20l-26-11 -11 18 23 17c-4 9-6 18-7 28l-28 4v21l28 4c1 10 4 19 7 28l-23 17 11 18 26-11c6 8 13 14 20 20l-11 26 18 11 17-23c9 4 18 6 28 7l4 28h21l4-28c10-1 19-4 28-7l17 23 18-11 -11-26c8-6 14-13 20-20l26 11 11-18 -23-17c4-9 6-18 7-28L402 400zM265 463c-41 0-74-33-74-74 0-41 33-74 74-74 41 0 74 33 74 74C338 430 305 463 265 463z" />
-                                </g>
-                                <g>
-                                    <path x="-100,136" y="-100,136" class="small-shadow"
-                                        d="M210 246v-21l-29-4c-2-10-6-18-11-26l18-23 -15-15 -23 18c-8-5-17-9-26-11l-4-29H100l-4 29c-10 2-18 6-26 11l-23-18 -15 15 18 23c-5 8-9 17-11 26L10 225v21l29 4c2 10 6 18 11 26l-18 23 15 15 23-18c8 5 17 9 26 11l4 29h21l4-29c10-2 18-6 26-11l23 18 15-15 -18-23c5-8 9-17 11-26L210 246zM110 272c-20 0-37-17-37-37s17-37 37-37c20 0 37 17 37 37S131 272 110 272z" />
-                                </g>
-                                <g>
-                                    <path x="-100,136" y="-100,136" class="small"
-                                        d="M200 236v-21l-29-4c-2-10-6-18-11-26l18-23 -15-15 -23 18c-8-5-17-9-26-11l-4-29H90l-4 29c-10 2-18 6-26 11l-23-18 -15 15 18 23c-5 8-9 17-11 26L0 215v21l29 4c2 10 6 18 11 26l-18 23 15 15 23-18c8 5 17 9 26 11l4 29h21l4-29c10-2 18-6 26-11l23 18 15-15 -18-23c5-8 9-17 11-26L200 236zM100 262c-20 0-37-17-37-37s17-37 37-37c20 0 37 17 37 37S121 262 100 262z" />
-                                </g>
-                                <g>
-                                    <path x="-173,694" y="-173,694" class="large"
-                                        d="M635 184v-21l-29-4c-1-10-3-19-6-28l25-14 -8-19 -28 7c-5-8-10-16-16-24L592 58l-15-15 -23 17c-7-6-15-11-24-16l7-28 -19-8 -14 25c-9-3-18-5-28-6L472 0h-21l-4 29c-10 1-19 3-28 6L405 9l-19 8 7 28c-8 5-16 10-24 16l-23-17L331 58l17 23c-6 7-11 15-16 24l-28-7 -8 19 25 14c-3 9-5 18-6 28l-29 4v21l29 4c1 10 3 19 6 28l-25 14 8 19 28-7c5 8 10 16 16 24l-17 23 15 15 23-17c7 6 15 11 24 16l-7 28 19 8 14-25c9 3 18 5 28 6l4 29h21l4-29c10-1 19-3 28-6l14 25 19-8 -7-28c8-5 16-10 24-16l23 17 15-15 -17-23c6-7 11-15 16-24l28 7 8-19 -25-14c3-9 5-18 6-28L635 184zM461 284c-61 0-110-49-110-110S401 64 461 64s110 49 110 110S522 284 461 284z" />
-                                </g>
-                                <g>
-                                    <path x="-136,996" y="-136,996" class="medium"
-                                        d="M392 390v-21l-28-4c-1-10-4-19-7-28l23-17 -11-18L342 313c-6-8-13-14-20-20l11-26 -18-11 -17 23c-9-4-18-6-28-7l-4-28h-21l-4 28c-10 1-19 4-28 7l-17-23 -18 11 11 26c-8 6-14 13-20 20l-26-11 -11 18 23 17c-4 9-6 18-7 28l-28 4v21l28 4c1 10 4 19 7 28l-23 17 11 18 26-11c6 8 13 14 20 20l-11 26 18 11 17-23c9 4 18 6 28 7l4 28h21l4-28c10-1 19-4 28-7l17 23 18-11 -11-26c8-6 14-13 20-20l26 11 11-18 -23-17c4-9 6-18 7-28L392 390zM255 453c-41 0-74-33-74-74 0-41 33-74 74-74 41 0 74 33 74 74C328 420 295 453 255 453z" />
-                                </g>
-                            </svg>
-                        </div>
+    <div class="section sectionHome">
+        <div class="container">
+            <div class="row grey-text text-darken-4 lax" data-lax-translate-y="0 0, 400 -50">
+                <div class="col l6 hide-on-med-and-down">
+                    <h3>Vamos começar</h3>
+                    <blockquote>
+                        GestClass é uma plataforma que ajuda o corpo docente escolar a gerenciar o ambiene
+                        educacional com mais facilidade
+                    </blockquote>
+                    <a href="contratacao.php" class="btn-flat btn-large btnDark btnHome">
+                        Quero fazer parte
+                    </a>
+                    <a href="#recursos" class="btn-flat btn-large btnDarkFill btnHome">
+                        Veja mais
+                    </a>
+                </div>
+                <div class="col l6 m8 s12 offset-m2">
+                    <div class="card-panel z-depth-5">
+                        <form class="" action="" method="post">
+                            <h4>Faça seu logon</h4>
+                            <div class="input-field">
+                                <input name="emailLogin" type="email" placeholder="Email" class="inputDark">
+                            </div>
+                            <div class="input-field right-align">
+                                <input name="senhaLogin" type="password" placeholder="Senha" class="inputDark">
+                                <a href="" class="linkAzul">Esqueci minha senha</a>
+                            </div>
+                                <button class="btn btn-flat waves-effect waves-light btnDark btnBlock" type="submit" name="action">
+                                    Logar
+                                </button>
+                        </form>
                     </div>
                 </div>
-            </li>
-        </ul>
+            </div>
+        </div>
     </div>
 
-    <div class="secao center grey-text text-darken-4">
+    <div class="section center grey-text text-darken-4">
         <div class="container">
             <div class="row">
                 <div class="col s12 m12 l4">
@@ -172,7 +148,7 @@
         </div>
     </div>
 
-    <div class="scrollspy light-blue lighten-4 secao grey-text text-darken-4" id="sobre">
+    <div class="scrollspy light-blue lighten-4 section grey-text text-darken-4" id="sobre">
         <div class="container">
             <div class="row">
                 <h2>Sobre nós</h2>
@@ -188,7 +164,7 @@
         </div>
     </div>
 
-    <div class="scrollspy center secao">
+    <div class="scrollspy center section">
         <div class="container">
             <div class="row">
                 <div class="col s12 grey-text text-darken-4">
@@ -202,12 +178,12 @@
         </div>
     </div>
 
-    <div class="scrollspy light-blue lighten-4 secao grey-text text-darken-4" id="recursos">
+    <div class="scrollspy light-blue lighten-4 section grey-text text-darken-4" id="recursos">
         <div class="container">
             <div class="row">
                 <h2 class="">Recursos</h2>
                 <div class="col s12 m6">
-                    
+
                 </div>
                 <div class="col s12 m6">
                     <ul class="collapsible">
@@ -226,7 +202,8 @@
                         </li>
                         <li>
                             <div class="collapsible-header flow-text">
-                                <i class="fas fa-chalkboard-teacher"></i><span class="titleCollapsible">Professores</span>
+                                <i class="fas fa-chalkboard-teacher"></i><span
+                                    class="titleCollapsible">Professores</span>
                             </div>
                             <div class="collapsible-body white">
                                 <ul>
@@ -244,7 +221,8 @@
                                 <ul>
                                     <li>Acesso ao boletim escolar</li>
                                     <li>Acessem sua frequência escolar</li>
-                                    <li>Possam ver o calendário escolar com as datas onde haverá ou não aula (reunião de pais, mestres, etc)
+                                    <li>Possam ver o calendário escolar com as datas onde haverá ou não aula (reunião de
+                                        pais, mestres, etc)
                                     </li>
                                     <li>Notificações da secretaria</li>
                                     <li>Urgências (advertências, anotações etc)</li>
@@ -260,7 +238,8 @@
                                     <li>Acesso ao boletim escolar</li>
                                     <li>Acessem a grade das matérias</li>
                                     <li>Acessem sua frequência escolar</li>
-                                    <li>Possam ver o calendário escolar com as datas onde haverá ou não aula (reunião de pais, mestres, etc)
+                                    <li>Possam ver o calendário escolar com as datas onde haverá ou não aula (reunião de
+                                        pais, mestres, etc)
                                     </li>
                                     <li>Notificações da secretaria</li>
                                 </ul>
@@ -315,7 +294,7 @@
         </div>
     </div>
 
-    <div class="scrollspy grey-text text-darken-4 secao" id="planos">
+    <div class="scrollspy grey-text text-darken-4 section" id="planos">
         <div class="container">
             <div class="row">
                 <h2>Planos</h2>
@@ -438,7 +417,8 @@
                         </div>
                         <div class="input-field right-align">
                             <input type="password" class="inputDark senhaLogin" name="senhaLogin" placeholder="Senha" />
-                            <span onclick="verSenha()" class="fa fa-fw fa-eye iconVerSenha btnVerSenha grey-text text-darken-4 right"></span>
+                            <span onclick="verSenha()"
+                                class="fa fa-fw fa-eye iconVerSenha btnVerSenha grey-text text-darken-4 right"></span>
                             <span class="helper-text red-text" id="spanSenha"></span>
                             <a href="recuperacaoSenha.php">Esqueci minha senha</a>
                         </div>
