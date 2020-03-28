8cd8fc13f23e1a75f2574ef374adda3780963ace
Hector Lima <hector.limaaa@gmail.com>
2020-03-28 13:17:31 -0300

    Hector e Eric - Inserção do tab para cadastro e alteração das notas

--- boletimCadastro.html.php
+++ boletimCadastro.html.php
@@ -13,127 +13,256 @@
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
         <script src="https://use.fontawesome.com/releases/v5.9.0/js/all.js"></script>
         <link rel="stylesheet" type="text/css" href="css/default.css" />
+        <link rel="stylesheet" type="text/css" href="css/boletimCadastro.css" />
 
     </head>
 
     <body id="body_boletimCadastro">
 
-        <?php require_once 'reqHeader.php' ?>        
-                    
-        <div class="container col s12 m12 l12" id="container_boletimCadastro">             
+        <?php require_once 'reqHeader.php' ?>
 
-            <h4>Cadastro de Notas</h4>
+
+
+        <div class="container col s12 m12 l12" id="container_boletimCadastro">
+
+          <div class="row">
+              <div class="col s12 m12 l12">
+                  <ul id="tabs-swipe-demo" class="tabs blue lighten-3">
+                      <li class="tab col s3 m6 l6 "><a href="#cadastro">Cadastro de notas</a></li>
+                      <li class="tab col s3 m6 l6 "><a href="#alteracao">Alteração de notas</a></li>
+                  </ul>
+              </div>
+
             <br>
-            <div class="row">
-                <div class="input-field col s12 m6 l6 validate">
-                    <i class="material-icons prefix blue-icon">content_copy</i>
-                    <select id="nome_tipo_turma">
-                    <option value="" disabled selected>Selecione a Turma</option>            
-                    <option value="4">8º ano A</option>
-                    <option value="5">9º ano A</option>
-                    <option value="1">1º ano A</option>
-                    <option value="2">2º ano A</option>
-                    <option value="3">3º ano A</option>
-                    </select>
-                    <label id="lbl">Turma</label>                    
-                </div>
+            <br>
+            <div id="cadastro" class="col s12 m12 l12">
+              <h4 class="center">Cadastro de Notas</h4>
+              <br>
+              <div class="row">
+                  <div class="input-field col s12 m6 l6 validate">
+                      <i class="material-icons prefix blue-icon">content_copy</i>
+                      <select id="nome_tipo_turma">
+                      <option value="" disabled selected>Selecione a Turma</option>
+                      <option value="4">8º ano A</option>
+                      <option value="5">9º ano A</option>
+                      <option value="1">1º ano A</option>
+                      <option value="2">2º ano A</option>
+                      <option value="3">3º ano A</option>
+                      </select>
+                      <label id="lbl">Turma</label>
+                  </div>
 
-                <div class="input-field col s12 m6 l6 validate">
-                    <i class="material-icons prefix blue-icon">library_books</i>
-                    <select id="nome_tipo_turma">
-                    <option value="" disabled selected>Selecione a Disciplina</option>            
-                    <option value="4">Portugê<s></s></option>
-                    <option value="5">Inglês</option>
-                    <option value="1">História</option>
-                    </select>
-                    <label id="lbl">Turma</label>
-                </div>
-            </div>
+                  <div class="input-field col s12 m6 l6 validate">
+                      <i class="material-icons prefix blue-icon">library_books</i>
+                      <select id="nome_tipo_turma">
+                      <option value="" disabled selected>Selecione a Disciplina</option>
+                      <option value="4">Portugê<s></s></option>
+                      <option value="5">Inglês</option>
+                      <option value="1">História</option>
+                      </select>
+                      <label id="lbl">Turma</label>
+                  </div>
+              </div>
 
-            <div class="row">
-                <div class="input-field col s12 m6 l6">
-                    <i class="material-icons prefix blue-icon">border_color</i>
-                    <input placeholder="Dê um nome a atividade" type="text" >
-                    <label id="lbl">Nome atividade</label>
-                </div>
+              <div class="row">
+                  <div class="input-field col s12 m6 l6">
+                      <i class="material-icons prefix blue-icon">border_color</i>
+                      <input placeholder="Dê um nome a atividade" type="text" >
+                      <label id="lbl">Nome atividade</label>
+                  </div>
 
-                <div class="file field input-field col s12 m6 l6">
-                    <i class="material-icons prefix blue-icon">event</i>
-                    <input  placeholder="Ano/Mes/Dia" type="text" class="datepicker validate">
-                    <label id="lbl">Data da atividade</label>
-                </div>                
-            </div>
+                  <div class="file field input-field col s12 m6 l6">
+                      <i class="material-icons prefix blue-icon">event</i>
+                      <input  placeholder="Ano/Mes/Dia" type="text" class="datepicker validate">
+                      <label id="lbl">Data da atividade</label>
+                  </div>
+              </div>
 
-            <table class="highlight centered">
-                <thead>
-                    <tr>
-                        <th>Nome</th>
-                        <th>Nota</th>
-                        <th>Observações</th>
-                    </tr>
-                </thead>
+              <table class="highlight centered">
+                  <thead>
+                      <tr>
+                          <th>Nome</th>
+                          <th>Nota</th>
+                          <th>Observações</th>
+                      </tr>
+                  </thead>
 
-                <tbody>
-                    <tr>
-                        <td>Nome(Aluno)</td>
-                        <td>
-                            <label>
-                                <input type="number" data-mask="00.00" placeholder="Ex: 09.50"/>
-                                <span></span>
-                            </label>
-                        </td>
-                        <td>
-                            <input type="text" placeholder="Observações . . ."/>
-                        </td>
-                    </tr>
+                  <tbody>
+                      <tr>
+                          <td>Nome(Aluno)</td>
+                          <td>
+                              <label>
+                                  <input type="number" data-mask="00.00" placeholder="Ex: 09.50"/>
+                                  <span></span>
+                              </label>
+                          </td>
+                          <td>
+                              <input type="text" placeholder="Observações . . ."/>
+                          </td>
+                      </tr>
 
-                    <tr>
-                        <td>Nome(Aluno)</td>
-                        <td>
-                            <label>
-                                <input type="number" data-mask="00.00" placeholder="Ex: 09.50"/>
-                                <span></span>
-                            </label>
-                        </td>
-                        <td>
-                            <input type="text" placeholder="Observações . . ."/>
-                        </td>
-                    </tr>
+                      <tr>
+                          <td>Nome(Aluno)</td>
+                          <td>
+                              <label>
+                                  <input type="number" data-mask="00.00" placeholder="Ex: 09.50"/>
+                                  <span></span>
+                              </label>
+                          </td>
+                          <td>
+                              <input type="text" placeholder="Observações . . ."/>
+                          </td>
+                      </tr>
 
-                    <tr>
-                        <td>Nome(Aluno)</td>
-                        <td>
-                            <label>
-                                <input type="number" data-mask="00.00" placeholder="Ex: 09.50"/>
-                                <span></span>
-                            </label>
-                        </td>
-                        <td>
-                            <input type="text" placeholder="Observações . . ."/>
-                        </td>
-                    </tr>
+                      <tr>
+                          <td>Nome(Aluno)</td>
+                          <td>
+                              <label>
+                                  <input type="number" data-mask="00.00" placeholder="Ex: 09.50"/>
+                                  <span></span>
+                              </label>
+                          </td>
+                          <td>
+                              <input type="text" placeholder="Observações . . ."/>
+                          </td>
+                      </tr>
 
-                <tr>
-                    <td>Nome(Aluno)</td>
-                    <td>
-                            <label>
-                                <input type="number" data-mask="00.00" placeholder="Ex: 09.50"/>
-                                <span></span>
-                            </label>
-                        </td>                        
-                        <td>
-                            <input type="text" placeholder="Observações . . ."/>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
+                  <tr>
+                      <td>Nome(Aluno)</td>
+                      <td>
+                              <label>
+                                  <input type="number" data-mask="00.00" placeholder="Ex: 09.50"/>
+                                  <span></span>
+                              </label>
+                          </td>
+                          <td>
+                              <input type="text" placeholder="Observações . . ."/>
+                          </td>
+                      </tr>
+                  </tbody>
+              </table>
 
-            <div class="input-field right">
-                <button id="btnTableChamada" type="submit" class="btn-flat btnLightBlue">
-                    <i class="material-icons left">send</i>Enviar
-                </button>
-            </div>
-        </div>            
-        
+              <div class="input-field right">
+                  <button id="btnTableChamada" type="submit" class="btn-flat btnLightBlue">
+                      <i class="material-icons left">send</i>Enviar
+                  </button>
+              </div>
+          </div>
+        </div>
 
+        <div id="alteracao" class="col s12 m12 l12">
+          <h4 class="center">Alteraçao de Notas</h4>
+          <br>
+          <div class="row">
+              <div class="input-field col s12 m6 l6 validate">
+                  <i class="material-icons prefix blue-icon">content_copy</i>
+                  <select id="nome_tipo_turma">
+                  <option value="" disabled selected>Selecione a Turma</option>
+                  <option value="4">8º ano A</option>
+                  <option value="5">9º ano A</option>
+                  <option value="1">1º ano A</option>
+                  <option value="2">2º ano A</option>
+                  <option value="3">3º ano A</option>
+                  </select>
+                  <label id="lbl">Turma</label>
+              </div>
+
+              <div class="input-field col s12 m6 l6 validate">
+                  <i class="material-icons prefix blue-icon">library_books</i>
+                  <select id="nome_tipo_turma">
+                  <option value="" disabled selected>Selecione a Disciplina</option>
+                  <option value="4">Portugê<s></s></option>
+                  <option value="5">Inglês</option>
+                  <option value="1">História</option>
+                  </select>
+                  <label id="lbl">Turma</label>
+              </div>
+          </div>
+
+          <div class="row">
+              <div class="input-field col s12 m6 l6">
+                  <i class="material-icons prefix blue-icon">border_color</i>
+                  <input placeholder="Dê um nome a atividade" type="text" >
+                  <label id="lbl">Nome atividade desempenhada</label>
+              </div>
+
+              <div class="file field input-field col s12 m6 l6">
+                  <i class="material-icons prefix blue-icon">event</i>
+                  <input  placeholder="Ano/Mes/Dia" type="text" class="datepicker validate">
+                  <label id="lbl">Data da atividade realizada</label>
+              </div>
+          </div>
+
+          <table class="highlight centered">
+              <thead>
+                  <tr>
+                      <th>Nome</th>
+                      <th>Nota</th>
+                      <th>Observações</th>
+                  </tr>
+              </thead>
+
+              <tbody>
+                  <tr>
+                      <td>Nome(Aluno)</td>
+                      <td>
+                          <label>
+                              <input type="number" data-mask="00.00" placeholder="Ex: 09.50"/>
+                              <span></span>
+                          </label>
+                      </td>
+                      <td>
+                          <input type="text" placeholder="Observações . . ."/>
+                      </td>
+                  </tr>
+
+                  <tr>
+                      <td>Nome(Aluno)</td>
+                      <td>
+                          <label>
+                              <input type="number" data-mask="00.00" placeholder="Ex: 09.50"/>
+                              <span></span>
+                          </label>
+                      </td>
+                      <td>
+                          <input type="text" placeholder="Observações . . ."/>
+                      </td>
+                  </tr>
+
+                  <tr>
+                      <td>Nome(Aluno)</td>
+                      <td>
+                          <label>
+                              <input type="number" data-mask="00.00" placeholder="Ex: 09.50"/>
+                              <span></span>
+                          </label>
+                      </td>
+                      <td>
+                          <input type="text" placeholder="Observações . . ."/>
+                      </td>
+                  </tr>
+
+              <tr>
+                  <td>Nome(Aluno)</td>
+                  <td>
+                          <label>
+                              <input type="number" data-mask="00.00" placeholder="Ex: 09.50"/>
+                              <span></span>
+                          </label>
+                      </td>
+                      <td>
+                          <input type="text" placeholder="Observações . . ."/>
+                      </td>
+                  </tr>
+              </tbody>
+          </table>
+
+          <div class="input-field right">
+              <button id="btnTableChamada" type="submit" class="btn-flat btnLightBlue">
+                  <i class="material-icons left">send</i>Enviar
+              </button>
+          </div>
+      </div>
+    </div>
+    <script src="js/default.js"></script>
 <?php require_once 'reqFooter.php' ?>
