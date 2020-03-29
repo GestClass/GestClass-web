1a353a7e03af2581e946a086852b831fb01faf7d
Monique Correia <nick_oliveira2002@hotmail.com>
2020-03-28 22:15:40 -0300

    teste

---
+++ teste.php
@@ -0,0 +1,95 @@
+<head><style>
+    .teste { 
+        width: 100%;
+        color: red;
+    }
+</style></head>
+<table class="centered striped">
+<thead>
+  <tr>
+      <th class="teste" >COMPONENTE CURRICULAR</th>
+      <th colspan="2"> <?php echo "1ºBIMESTRE";?></th>
+      <th colspan="2">2ºBIMESTRE</th>
+      <th colspan="2">3ºBIMESTRE</th>
+      <th colspan="2">4ºBIMESTRE</th>
+      <th colspan="2"> RESULTADO FINAL</th>
+  </tr>
+</thead>
+
+<tbody>
+  <tr>
+    <th> DISCIPLINAS</th>
+    <th>n</th>
+    <th>f</th>
+    <th>n</th>
+    <th>f</th>
+    <th>n</th>
+    <th>f</th>
+    <th>n</th>
+    <th>f</th>
+    <th>MÉDIA</th>
+    <TH> FALTAS</TH>
+  </tr>
+  <tr>
+  <td>GEOGRAFIA</td>
+    <td>9.55</td>
+    <td>2</td>
+    <td>10</td>
+    <td>1</td>
+    <td>10</td>
+    <td>-</td>
+    <td>10</td>
+    <td>5</td>
+    <td>8.7</td>
+  </tr>
+  <tr>
+  <td>GEOGRAFIA</td>
+    <td>8.55</td>
+    <td>4</td>
+    <td>10</td>
+    <td>3</td>
+    <td>10</td>
+    <td>-</td>
+    <td>10</td>
+    <td>5</td>
+    <td>8.7</td>
+  </tr>
+  <tr>
+  <td>GEOGRAFIA</td>
+    <td>7.55</td>
+    <td>4</td>
+    <td>10</td>
+    <td>3</td>
+    <td>10</td>
+    <td>8.7</td>
+    <td>10</td>
+    <td>8.7</td>
+    <td>8.7</td>
+  </tr>
+  <tr>
+  <td>GEOGRAFIA</td>
+    <td>4.55</td>
+    <td>9.87</td>
+    <td>10</td>
+    <td>8.7</td>
+    <td>10</td>
+    <td>8.7</td>
+    <td>10</td>
+    <td>8.7</td>
+    <td>8.7</td>
+  </tr>
+  <tr>
+  <td>GEOGRAFIA</td>
+    <td>4.55</td>
+    <td>9.87</td>
+    <td>10</td>
+    <td>8.7</td>
+    <td>10</td>
+    <td>8.7</td>
+    <td>10</td>
+    <td>8.7</td>
+    <td>8.7</td>
+   
+  </tr>
+</tbody>
+</table>
