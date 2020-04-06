9e44741c6c687117d00724c90207496e0543c9fc
Caio Fonseca <fcaio8137@gmail.com>
2020-04-06 04:14:24 -0300

    Caio - Back-end cadastro Secretario

--- php/cadastrarSecretaria.php
+++ php/cadastrarSecretaria.php
@@ -68,16 +68,15 @@
 			move_uploaded_file($temp, $caminho);
 			
 			$query = $conn->prepare("INSERT INTO secretario (nome_secretario, foto, cep, numero, complemento, rg, cpf, email, senha, celular, telefone, fk_id_tipo_usuario_secretario, fk_id_escola_secretario)
-            VALUES ('{$nome}', '{$nome_imagem}', '{cep}', '{numero}', '{complemento}','{$rg}', '{$cpf_secretario}', '{$email}', '{$senha}', '{$celular}','{$telefone}','3' ,'{$id_escola}')");
+            VALUES ('{$nome}', '{$nome_imagem}','{$cep}','{$numero}','{$complemento}','{$rg}', '{$cpf_secretario}', '{$email}', '{$senha}', '{$celular}','{$telefone}','3' ,'{$id_escola}')");
 
             if ($query->execute()) {
-
                 echo "<script>alert('Secretario cadastrado com sucesso');
-                        window.location='../homeSecretaria.html.php';
+                    window.location='../homeSecretaria.html.php';
                      </script>";
             }else{
 				echo "<script>alert('Erro: Secretario não foi cadastrado');
-				history.back();;
+				 history.back();;
 				 </script>";
 			
 
