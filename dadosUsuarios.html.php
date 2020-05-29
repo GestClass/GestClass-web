20b82bb4386f732ab70d5b5c9504a5fb4e3325dc
Hector Lima <hector.limaaa@gmail.com>
2020-05-29 01:07:35 -0300

    Hector - Dados dos usuários - select dos campos

--- dadosUsuarios.html.php
+++ dadosUsuarios.html.php
@@ -20,38 +20,49 @@
 
 <body>
 
-    <?php require_once 'reqMenuAdm.php' ?>
+    <?php
+
+    include_once 'php/conexao.php';
+    
+    
+    $query_alunos = $conn->prepare("SELECT * FROM aluno WHERE fk_id_escola_aluno = 1 AND fk_id_turma_aluno = 16 AND RA = 1");
+    $query_alunos->execute();
+    $dados_alunos = $query_alunos->fetch(PDO::FETCH_ASSOC)
+    
+    
+    ?>
 
     <div class="container col s12 m12 l12 ">
 
         <h5>Dados Pessoais</h5>
         <?php 
-            $tipo_usuario = "secretaria";
+            $tipo_usuario = "aluno";
             if($tipo_usuario == "aluno") { ?>
         <form id="aluno" method="POST" action="php/cadastrarAluno.php" enctype="multipart/form-data">
             <div class="row">
                 <div class="input-field col s12 m9 l9">
                     <i class="material-icons prefix blue-icon">account_circle</i>
-                    <input name="nome" id="nome_aluno" type="text" value="Teste" class="validate " readonly>
+                    <input name="nome" id="nome_aluno" type="text" value="<?php echo $dados_alunos['nome_aluno']?>" 
+                    class="validate " readonly>
                     <label id="lbl" for="first_name">Nome Aluno</label>
                 </div>
             </div>
             <div class="row">
                 <div class="input-field col s6 m4 l3">
                     <i class="material-icons prefix blue-icon">cake</i>
-                    <input name="data_nascimento" id="data_nascimento" value="1994/09/27" type="text" class="validate"
-                        readonly>
+                    <input name="data_nascimento" id="data_nascimento" value="<?php echo $dados_alunos['data_nascimento']?>" type="text" class="validate" 
+                    data-mask="" readonly>
                     <label id="lbl">Data Nascimento</label>
                 </div>
                 <div class="input-field col s6 m4 l3">
                     <i class="material-icons prefix blue-icon">ballot</i>
-                    <input name="ra" id="RA" placeholder="8956478-9" value="Teste" type="text" class="validate"
+                    <input name="ra" id="RA" placeholder="8956478-9" value=<?php echo $dados_alunos['RA']?> data-mask="0000000-0" type="text" class="validate"
                         readonly>
                     <label id="lbl" for="first_name">RA</label>
                 </div>
                 <div class="input-field col s6 m4 l3">
                     <i class="material-icons prefix blue-icon">ballot</i>
-                    <input name="rg" id="RG" value="Teste" type="tel" class="validate " readonly>
+                    <input name="rg" id="RG" value="<?php echo $dados_alunos['RG']?>" type="tel" class="validate " data-mask="00.000.000-0 readonly>
                     <label id="lbl" for="first_name">RG</label>
                 </div>
                 <div class="input-field col s6 m6 l4">
@@ -332,10 +343,89 @@
                         </div>
                     </form>
                     <?php } 
-            elseif($tipo_usuario == "secretaria")?>
+                        elseif($tipo_usuario == "diretor") { ?>
+
+                    <div class="container ">
+                        <form id="adicionarEscola" class="col s12" method="POST" action="php/cadastrarDiretor.php"
+                            enctype="multipart/form-data">
+                            <div class="col s12 m12 l12">
+                                <div class="row">
+                                </div>
+                                <div class="input-field col s12 m9 l9">
+                                    <i class="material-icons prefix blue-icon">account_circle</i>
+                                    <input id="icon_titulo" type="text" name="nome_diretor" id="nome_diretor"
+                                        value="Teste" readonly class="validate">
+                                    <label for="icon_titulo">Nome Diretor</label>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="input-field col s6 m6 l4">
+                                    <i class="material-icons prefix blue-icon">ballot</i>
+                                    <input name="rg" id="rg" type="tel" value="Teste" readonly class="validate">
+                                    <label id="lbl" for="icon_telephone">RG</label>
+                                </div>
+                                <div class="input-field col s6 m6 l4">
+                                    <i class="material-icons prefix blue-icon">ballot</i>
+                                    <input name="cpf" id="CPF" type="tel" value="Teste" readonly class="validate">
+                                    <label id="lbl" for="icon_telephone">CPF</label>
+                                </div>
+                                <div class="input-field col s12 m4 l4">
+                                    <i class="material-icons prefix blue-icon">alternate_email</i>
+                                    <input name="email" id="email" type="tel" value="Teste" readonly class="validate">
+                                    <label id="lbl" for="icon_telephone">Email</label>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="input-field col s12 m4 l4">
+                                    <i class="material-icons prefix blue-icon">location_on</i>
+                                    <input name="cep" id="cep" value="Teste" readonly type="text" class="validate">
+                                    <label id="lbl" for="first_name">CEP</label>
+                                </div>
+                                <div id="a" class="input-field col s10 m4 l4">
+                                    <input id="cidade" value="Teste" readonly type="text" class="validate">
+                                    <label id="lbl" for="first_name">Cidade</label>
+                                </div>
+                                <div id="a" class="input-field col s10 m4 l4">
+                                    <input id="bairro" value="Teste" readonly type="text" class="validate">
+                                    <label id="lbl" for="first_name">Bairro</label>
+                                </div>
+                                <div id="a" class="input-field col s10 m4 l4">
+                                    <input id="rua" value="Teste" readonly type="text" class="validate">
+                                    <label id="lbl" for="first_name">Rua</label>
+                                </div>
+                                <div id="a" class="input-field col s10 m2 l4">
+                                    <input name="numero" id="numero" value="Teste" readonly type="tel"
+                                        class="validate ">
+                                    <label id="lbl" for="first_name">Nº</label>
+                                </div>
+                                <div id="a" class="input-field col s10 m6 l4">
+                                    <input name="complemento" id="complemento" value="Teste" readonly type="tel"
+                                        class="validate ">
+                                    <label id="lbl" for="first_name">Complemento</label>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="input-field col s12 m6 l6">
+                                    <i class="material-icons prefix blue-icon">smartphone</i>
+                                    <input name="celular" id="celular" type="tel" value="Teste" readonly
+                                        class="validate">
+                                    <label id="lbl" for="icon_telephone">Celular</label>
+                                </div>
+                                <div class="input-field col s12 m6 l6">
+                                    <i class="material-icons prefix blue-icon">call</i>
+                                    <input name="telefone" id="telefone" type="tel" value="Teste" readonly
+                                        class="validate">
+                                    <label id="lbl" for="icon_telephone">Telefone</label>
+                                </div>
+                            </div>
+
+                    </div>
+                    </form>
                 </div>
 
-                <script src="js/query-3.3.1.min.js"></script>
-                <script src="js/default.js"></script>
 
-                <?php require_once 'reqFooter.php' ?>
+            </div>
+            <?php }?>
+
+
+            <?php include_once 'reqFooter.php'?>
