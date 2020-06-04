9aa489b63b28a4163022457bbd348bbf4384cc74
Hector Lima <hector.limaaa@gmail.com>
2020-06-04 18:51:05 -0300

    Hector - inicio do desenvonvilmento da alteração dos dados dos usuários

--- alteracaoDados.html.php
+++ alteracaoDados.html.php
@@ -36,18 +36,20 @@
         require_once 'reqProfessor.php';
     }
     
-    ?>
-    <h4 class="center">Dados pessoais</h4>
+    
+    
 
-    <?php
+    
 
-    $tipo_usuario = "aluno";
+    $tipo_usuario = $_POST['tipo_conta'];
+   
 
     if ($tipo_usuario == "aluno") {
         $query_alunos = $conn->prepare("SELECT * FROM aluno WHERE RA = 1");
         $query_alunos->execute();
         while ($dados_alunos = $query_alunos->fetch(PDO::FETCH_ASSOC)) {
-            ?>
+            ?> 
+            <h4 class="center">Alteração de dados </h4>
     <div class="container col s12 m12 l12 ">
 
         <form id="aluno" method="POST" action="alteracaoDados.html.php" enctype="multipart/form-data">
@@ -55,7 +57,7 @@
                 <div class="input-field col s12 m9 l6">
                     <i class="material-icons prefix blue-icon">account_circle</i>
                     <input name="nome" id="nome_aluno" type="text" value="<?php echo $dados_alunos['nome_aluno']?>"
-                        class="validate " readonly>
+                        class="validate ">
                     <label id="lbl" for="first_name">Nome Aluno</label>
                 </div>
             </div>
@@ -63,53 +65,53 @@
                 <div class="input-field col s6 m4 l5">
                     <i class="material-icons prefix blue-icon">cake</i>
                     <input name="data_nascimento" id="data_nascimento"
-                        value="<?php echo $dados_alunos['data_nascimento']?>" type="date" class="validate" readonly>
+                        value="<?php echo $dados_alunos['data_nascimento']?>" type="date" class="data-picker validate">
                     <label id="lbl">Data Nascimento</label>
                 </div>
                 <div class="input-field col s6 m4 l5">
                     <i class="material-icons prefix blue-icon">ballot</i>
                     <input name="ra" id="RA" placeholder="8956478-9" value=<?php echo $dados_alunos['RA']?>
-                        data-mask="0000000-0" type="text" class="validate" readonly>
+                        data-mask="0000000-0" type="text" class="validate">
                     <label id="lbl" for="first_name">RA</label>
                 </div>
                 <div class="input-field col s6 m4 l5">
                     <i class="material-icons prefix blue-icon">ballot</i>
                     <input name="rg" id="RG" value="<?php echo $dados_alunos['RG']?>" type="tel" class="validate "
-                        data-mask="00.000.000-0" readonly>
+                        data-mask="00.000.000-0">
                     <label id="lbl" for="first_name">RG</label>
                 </div>
                 <div class="input-field col s6 m6 l5">
                     <i class="material-icons prefix blue-icon">ballot</i>
                     <input name="cpf" id="CPF" value="<?php echo $dados_alunos['cpf']?>" type="text" class="validate "
-                        readonly>
+                        >
                     <label id="lbl" for="first_name">CPF</label>
                 </div>
             </div>
             <div class="row">
                 <div class="input-field col s12 m3 l3">
                     <i class="material-icons prefix blue-icon">location_on</i>
-                    <input name="cep" id="cep" value="teste" readonly type="text" class="validate">
+                    <input name="cep" id="cep" value="teste" type="text" class="validate">
                     <label id="lbl" for="first_name">CEP</label>
                 </div>
                 <div id="a" class="input-field col s10 m2 l3">
-                    <input id="cidade" value="teste" readonly type="text" class="validate">
+                    <input id="cidade" value="teste" type="text" class="validate">
                     <label id="lbl" for="first_name">Cidade</label>
                 </div>
                 <div id="a" class="input-field col s10 m2 l3">
-                    <input id="bairro" value="Teste" readonly type="text" class="validate">
+                    <input id="bairro" value="Teste"type="text" class="validate">
                     <label id="lbl" for="first_name">Bairro</label>
                 </div>
                 <div id="a" class="input-field col s10 m2 l6">
                     <i class="material-icons prefix blue-icon">edit_road</i>
-                    <input id="rua" value="teste" readonly type="text" class="validate">
+                    <input id="rua" value="teste" type="text" class="validate">
                     <label id="lbl" for="first_name">Rua</label>
                 </div>
                 <div id="a" class="input-field col s10 m1 l1">
-                    <input name="numero" id="numero" value="teste" readonly type="tel" class="validate ">
+                    <input name="numero" id="numero" value="teste" type="tel" class="validate ">
                     <label id="lbl" for="first_name">Nº</label>
                 </div>
                 <div id="a" class="input-field col s10 m2 l3">
-                    <input name="complemento" id="complemento" value="teste" readonly type="tel" class="validate ">
+                    <input name="complemento" id="complemento" value="teste" type="tel" class="validate ">
                     <label id="lbl" for="first_name">Complemento</label>
                 </div>
             </div>
@@ -123,13 +125,13 @@
                 <div class="input-field col s12 m6 l4">
                     <i class="material-icons prefix blue-icon">smartphone</i>
                     <input name="celular" id="celular" value="<?php echo $dados_alunos['celular']?>" type="tel"
-                        data-mask="(00) 00000-0000" class="validate " readonly>
+                        data-mask="(00) 00000-0000" class="validate ">
                     <label id="lbl" for="first_name">Celular</label>
                 </div>
                 <div class="input-field col s12 m6 l4">
                     <i class="material-icons prefix blue-icon">call</i>
                     <input name="telefone" id="telefone" value="<?php echo $dados_alunos['telefone']?>" type="tel"
-                        data-mask="(00) 0000-0000" class="validate" readonly>
+                        data-mask="(00) 0000-0000" class="validate">
                     <label id="lbl" for="first_name">Telefone</label>
                 </div>
             </div>
@@ -137,19 +139,14 @@
                 <div class="input-field col s12 m12 l2 validate">
                     <i class="material-icons prefix blue-icon">school</i>
                     <input name="telefone" id="telefone" type="text" class="validate"
-                        value="<?php echo $dados_alunos['fk_id_turma_aluno']?>" readonly>
+                        value="<?php echo $dados_alunos['fk_id_turma_aluno']?>">
                     <label id="lbl">Turma</label>
-                </div>
-                <div>
-                    <input type="text" name="tipo_conta" value="aluno" hidden>
                 </div>
             </div>
 
             <div class="row">
-                <input type="submit" class="waves-effect waves-light btn blue" value="Alterar dados">
+                <input type="submit" class="waves-effect waves-light btn blue" value="Salvar Alterações">
 
-                <a href="dadosResponsaveis.html.php" class="waves-effect waves-light btn blue"><i
-                        class="material-icons left">people_alt</i>Responsáveis</a>
             </div>
         </form>
 
