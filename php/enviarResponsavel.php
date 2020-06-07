2d7ae9ba8b00a20f05d969a9cfb23aa79e31e003
Ana Beatriz Rodrigues <anaana.abl@gmail.com>
2020-06-07 17:54:12 -0300

    Ana - Inserindo as mensagens dos pais/responsaveis envio para secretaria

--- php/enviarResponsavel.php
+++ php/enviarResponsavel.php
@@ -20,19 +20,38 @@
         `fk_recebimento_secretario_id_secretario`, `assunto`, `data`) 
         VALUES ('{$mensagem}', NULL, '{$id_usuario}', NULL, NULL, NULL, NULL, NULL, NULL, NULL, '{$id_secretario}', '{$assunto}', NOW())");
         $resultado = $inserirMensagem->execute();
-        
 
         if ($resultado == 1) {
-            alert('Cadastrado com Sucesso!!')
-                    window.location = '../mensagensResponsavel.html.php';
+            echo "<script>alert('Cadastrado com Sucesso!!');
+                    window.location = '../mensagensResponsavel.html.php';</script>";
         }else{
-            alert('Erro ao enviar a mensagem')
-                    history.back();
+            echo "<script>alert('Erro ao enviar a mensagem')
+                    history.back();</script>";
         }
 
-
+        
 
     }else{
+        
+        $selectDiretor = $conn->prepare("SELECT id_diretor FROM diretor where fk_id_escola_diretor =  $id_escola");
+        $selectDiretor->execute();
+        $diretor = $selectDiretor->fetch(PDO::FETCH_ASSOC);
+        $id_diretor = $diretor["id_diretor"];
+
+        $inserirMensagem = $conn->prepare("INSERT INTO `contato` (`mensagem`, `fk_envio_aluno_ra_aluno`, `fk_envio_responsavel_id_responsavel`, 
+        `fk_envio_professor_id_professor`, `fk_envio_diretor_id_diretor`, `fk_envio_secretario_id_secretario`, `fk_recebimento_aluno_ra_aluno`, 
+        `fk_recebimento_responsavel_id_responsavel`, `fk_recebimento_professor_id_professor`, `fk_recebimento_diretor_id_diretor`, 
+        `fk_recebimento_secretario_id_secretario`, `assunto`, `data`) 
+        VALUES ('{$mensagem}', NULL, '{$id_usuario}', NULL, NULL, NULL, NULL, NULL, NULL, '{$id_diretor}', NULL, '{$assunto}', NOW())");
+        $resultado = $inserirMensagem->execute();
+
+        if ($resultado == 1) {
+            echo "<script>alert('Cadastrado com Sucesso!!');
+                    window.location = '../mensagensResponsavel.html.php';</script>";
+        }else{
+            echo "<script>alert('Erro ao enviar a mensagem')
+                    history.back();</script>";
+        }
 
     }
 
