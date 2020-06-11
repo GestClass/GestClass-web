1fbdf7a6a17c98dc15cd7f5d83c2b7bcc7b84c80
Caio Fonseca <fcaio8137@gmail.com>
2020-06-11 01:24:15 -0300

    Caio: Enviando mensagens para os professores

--- mensagensDiretor.html.php
+++ mensagensDiretor.html.php
@@ -67,7 +67,7 @@
                     <?php while ($mensagens = $query_mensagem->fetch(PDO::FETCH_ASSOC)) { ?>
                         <tr>
                             <td><i class="small left material-icons blue-icon hide-on-small-only">email</i>
-                                <?php echo $mensagens["data_mensagem"] ?></td>
+                                <?php echo $mensagens["data_mensagem"]?></td>
                             <td><?php echo $mensagens["assunto"] ?></td>
                             <td><?php echo $mensagens["mensagem"] ?></td>
                         </tr>
@@ -77,39 +77,203 @@
         </div>
     </div>
 
-    <div id="modalMensagem" class="modal modal-fixed-footer">
+    <div id="modalEnviarAluno" class="modal modal-fixed-footer">
         <div class="modal-content">
             <h4>Nova Mensagem</h4><br>
             <div id="novaMensagem">
-                <form action="php/enviarDiretor.php" method="POST">
-                    <div class="row">
-                        <div class="input-field col s12 m12 l12">
-                            <select name="destinatario" id="mensagemDiretor">
-                                <option value="" disabled selected></option>
-                                <optgroup label="Aluno">
-                                    <option value="1">Uma Turma</option>
-                                    <option value="2">Aluno</option>
-                                <optgroup label="Professor">
-                                    <option value="3">Professor</option>
-                                <optgroup label="Responsável">
-                                    <option value="4">Responsável</option>
-                                <optgroup label="Secretaria">
-                                    <option value="5">Secretaria</option>
-                            </select>
-                            <label id="lbl" for="first_name">Escolha para quem deseja enviar a mensagem</label>
-                        </div>
-                    </div>
-
+                <form action="php/enviarSecretariaAluno.php" method="POST">
                     <div class="row">
                         <div class="col s12">
                             <div class="row">
                                 <div class="input-field col s12">
-                                    <input name="nome" type="text" id="autocomplete-input" placeholder="Digite o nome" class="autocomplete validate">
-                                    <label id="lbl" for="autocomplete-input">Nome Turma</label>
+                                    <input name="ra" type="text" placeholder="Digite RA do aluno" class="validate">
+                                    <label id="lbl">RA do aluno</label>
                                 </div>
                             </div>
                         </div>
                     </div>
+                    <div class="row">
+                        <div class="input-field col s12 m12 l12">
+                            <input name="assunto" id="assunto" placeholder="Digite o assunto" type="tel" class="validate ">
+                            <label id="lbl" for="first_name">Assunto</label>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="input-field col s12">
+                            <textarea name="mensagem" id="mensagem" placeholder="Digite sua mensagem" class="materialize-textarea"></textarea>
+                            <label id="lbl" for="textarea1">Digite a sua Mensagem</label>
+                        </div>
+                    </div>
+                    <div class="input-field right">
+                        <button btn="btncadastrar" value="formProfessor" id="btnFormContas" type="submit" class="btn-flat btnLightBlue"><i class="material-icons">send</i> Enviar</button>
+                    </div>
+                </form>
+
+            </div>
+        </div>
+        <div class="modal-footer">
+            <a href="#!" class="modal-close waves-effect waves-green btn-flat">Sair</a>
+        </div>
+    </div>
+
+    <div id="modalEnviarTurma" class="modal modal-fixed-footer">
+        <div class="modal-content">
+            <h4>Nova Mensagem</h4><br>
+            <div id="novaMensagem">
+                <form action="php/enviarDiretorTurmas.php" method="POST">
+                    <div class="row">
+                        <div class="input-field col s12 m4 l12">
+                            <select name="destinatario">
+                                <option value="" disabled selected></option>
+
+                                <?php
+
+                                $query_select_turmas = $conn->prepare("SELECT ID_turma FROM turma WHERE fk_id_escola_turma = $id_escola");
+                                $query_select_turmas->execute();
+
+                                while ($dados_turmas = $query_select_turmas->fetch(PDO::FETCH_ASSOC)) {
+                                    $id_turma = $dados_turmas["ID_turma"];
+
+                                    $query_select_turma = $conn->prepare("SELECT nome_turma FROM turma WHERE ID_turma = $id_turma");
+                                    $query_select_turma->execute();
+
+                                    while ($dados_turma_nome = $query_select_turma->fetch(PDO::FETCH_ASSOC)) {
+                                        $nome_turma = $dados_turma_nome["nome_turma"];
+
+                                ?>
+                                        <option value="<?php echo $id_turma ?>"><?php echo $nome_turma; ?></option>
+                                <?php
+                                    }
+                                } ?>
+                            </select>
+                            <label id="lbl" for="first_name">Escolha a turma para que deseja enviar a mensagem</label>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="input-field col s12 m12 l12">
+                            <input name="assunto" id="assunto" placeholder="Digite o assunto" type="tel" class="validate ">
+                            <label id="lbl" for="first_name">Assunto</label>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="input-field col s12">
+                            <textarea name="mensagem" id="mensagem" placeholder="Digite sua mensagem aqui" class="materialize-textarea"></textarea>
+                            <label id="lbl" for="textarea1">Digite a sua Mensagem</label>
+                        </div>
+                    </div>
+                    <div class="input-field right">
+                        <button btn="btncadastrar" value="formProfessor" id="btnFormContas" type="submit" class="btn-flat btnLightBlue"><i class="material-icons">send</i> Enviar</button>
+                    </div>
+
+                </form>
+            </div>
+        </div>
+        <div class="modal-footer">
+            <a href="#!" class="modal-close waves-effect waves-green btn-flat">Sair</a>
+        </div>
+    </div>
+
+    <div id="modalProfessor" class="modal modal-fixed-footer">
+        <div class="modal-content">
+            <h4>Nova Mensagem</h4><br>
+            <div id="novaMensagem">
+                <form action="php/enviarDiretorProf.php" method="POST">
+                    <div class="row">
+                        <div class="input-field col s12 m4 l12">
+                            <select name="destinatario">
+                                <option value="" disabled selected></option>
+
+                                <?php
+
+                                $query_select_professor = $conn->prepare("SELECT ID_professor FROM professor WHERE fk_id_escola_professor = $id_escola");
+                                $query_select_professor->execute();
+
+                                while ($dados_professor = $query_select_professor->fetch(PDO::FETCH_ASSOC)) {
+                                    $id_professor = $dados_professor["ID_professor"];
+
+                                    $query_select_professor = $conn->prepare("SELECT nome_professor FROM professor WHERE ID_professor = $id_professor");
+                                    $query_select_professor->execute();
+
+                                    while ($dados_professor_nome = $query_select_professor->fetch(PDO::FETCH_ASSOC)) {
+                                        $nome_professor = $dados_professor_nome["nome_professor"];
+
+                                ?>
+                                        <option value="<?php echo $id_professor ?>"><?php echo $nome_professor; ?></option>
+                                <?php
+                                    }
+                                } ?>
+                            </select>
+                            <label id="lbl" for="first_name">Escolha a turma para que deseja enviar a mensagem</label>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="input-field col s12 m12 l12">
+                            <input name="assunto" id="assunto" placeholder="Digite o assunto" type="tel" class="validate ">
+                            <label id="lbl" for="first_name">Assunto</label>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="input-field col s12">
+                            <textarea name="mensagem" id="mensagem" placeholder="Digite sua mensagem aqui" class="materialize-textarea"></textarea>
+                            <label id="lbl" for="textarea1">Digite a sua Mensagem</label>
+                        </div>
+                    </div>
+                    <div class="input-field right">
+                        <button btn="btncadastrar" value="formProfessor" id="btnFormContas" type="submit" class="btn-flat btnLightBlue"><i class="material-icons">send</i> Enviar</button>
+                    </div>
+
+                </form>
+            </div>
+        </div>
+        <div class="modal-footer">
+            <a href="#!" class="modal-close waves-effect waves-green btn-flat">Sair</a>
+        </div>
+    </div>
+
+    <div id="modalEnviarResponsavel" class="modal modal-fixed-footer">
+        <div class="modal-content">
+            <h4>Nova Mensagem</h4><br>
+            <div id="novaMensagem">
+                <form action="php/enviarDiretorRespon.php" method="POST">
+                    <div class="row">
+                        <div class="col s12">
+                            <div class="row">
+                                <div class="input-field col s12">
+                                    <input name="ra" type="text" placeholder="Pensar no que colocar aqui" class="validate">
+                                    <label id="lbl">RA do aluno</label>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="input-field col s12 m12 l12">
+                            <input name="assunto" id="assunto" placeholder="Digite o assunto" type="tel" class="validate ">
+                            <label id="lbl" for="first_name">Assunto</label>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="input-field col s12">
+                            <textarea name="mensagem" id="mensagem" placeholder="Digite sua mensagem" class="materialize-textarea"></textarea>
+                            <label id="lbl" for="textarea1">Digite a sua Mensagem</label>
+                        </div>
+                    </div>
+                    <div class="input-field right">
+                        <button btn="btncadastrar" value="formProfessor" id="btnFormContas" type="submit" class="btn-flat btnLightBlue"><i class="material-icons">send</i> Enviar</button>
+                    </div>
+                </form>
+
+            </div>
+        </div>
+        <div class="modal-footer">
+            <a href="#!" class="modal-close waves-effect waves-green btn-flat">Sair</a>
+        </div>
+    </div>
+
+    <div id="modalSecretaria" class="modal modal-fixed-footer">
+        <div class="modal-content">
+            <h4>Nova Mensagem</h4><br>
+            <div id="novaMensagem">
+                <form action="php/enviarDiretor.php" method="POST">
                     <div class="row">
                         <div class="input-field col s12 m12 l12">
                             <input name="assunto" id="assunto" placeholder="Digite o assunto" type="tel" class="validate ">
@@ -242,9 +406,13 @@
             <i class="large material-icons">add</i>
         </a>
         <ul>
-            <li><a href="#modalArquivados" class="modal-trigger btn-floating green accent-2 tooltipped" data-position="left" data-tooltip="Mensagens Arquivadas"><i class="material-icons">archive</i></a></li>
-            <li><a href="#modalEncaminharMensagem" class="modal-trigger btn-floating teal lighten-4 tooltipped" data-position="left" data-tooltip="Encaminhar para Todos"><i class="material-icons">email</i></a></li>
-            <li><a href="#modalMensagem" class="modal-trigger btn-floating yellow lighten-2 tooltipped" data-position="left" data-tooltip="Nova Mensagem"><i class="material-icons">email</i></a></li>
+            <li><a href="#modalArquivados" class="modal-trigger btn-floating green accent-5 tooltipped" data-position="left" data-tooltip="Mensagens Arquivadas"><i class="material-icons">archive</i></a></li>
+            <li><a href="#modalEncaminharMensagem" class="modal-trigger btn-floating indigo lighten-2 tooltipped" data-position="left" data-tooltip="Encaminhar para Todos"><i class="material-icons">record_voice_over</i></a></li>
+            <li><a href="#modalSecretaria" class="modal-trigger btn-floating grey darken-1 tooltipped" data-position="left" data-tooltip="Secretaria"><i class="material-icons">perm_identity</i></a></li>
+            <li><a href="#modalEnviarResponsavel" class="modal-trigger btn-floating black tooltipped" data-position="left" data-tooltip="Responsável"><i class="material-icons">supervisor_account</i></a></li>
+            <li><a href="#modalProfessor" class="modal-trigger btn-floating  yellow accent-2 tooltipped" data-position="left" data-tooltip="Professor"><i class="material-icons">portrait</i></a></li>
+            <li><a href="#modalEnviarTurma" class="modal-trigger btn-floating blue tooltipped" data-position="left" data-tooltip="Turmas"><i class="material-icons">school</i></a></li>
+            <li><a href="#modalEnviarAluno" class="modal-trigger btn-floating red lighten-2 tooltipped" data-position="left" data-tooltip="Aluno"><i class="material-icons">face</i></a></li>
         </ul>
     </div>
 
