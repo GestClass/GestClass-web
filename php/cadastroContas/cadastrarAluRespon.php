2f3778aaea3eb500115fa1fa9541932af7834a52
Caio Fonseca <fcaio8137@gmail.com>
2020-06-13 04:37:08 -0300

    Caio: ajustes para não ser cadatrado em branco as informações

--- php/cadastroContas/cadastrarAluRespon.php
+++ php/cadastroContas/cadastrarAluRespon.php
@@ -2,115 +2,110 @@
 
 include_once '../conexao.php';
 
-  	// Aluno
-	$id_escola = $_SESSION["id_escola"];
-	$id_tipo_usuario = $_SESSION["id_tipo_usuario"];
+$id_escola = $_SESSION["id_escola"];
+$id_tipo_usuario = $_SESSION["id_tipo_usuario"];
 
-	$query_select = $conn->prepare("select id_responsavel FROM responsavel ORDER BY id_responsavel DESC");
-	$query_select->execute();
-	$dados = $query_select->fetch(PDO::FETCH_ASSOC);
-	$id_responsavel = $dados["id_responsavel"];
-	
-	$rastro = $_POST["ra"];
+//Selecionando id dos responsavel que acabou de ser cadastrado
+$query_select = $conn->prepare("select id_responsavel FROM responsavel ORDER BY id_responsavel DESC");
+$query_select->execute();
+$dados = $query_select->fetch(PDO::FETCH_ASSOC);
+$id_responsavel = $dados["id_responsavel"];
+
+$rastro = $_POST["ra"];
+$nome = $_POST["nome"];
+$rg = $_POST["rg"];
+$cpf = $_POST["cpf"];
+$nascimento = $_POST["data_nascimento"];
+$email = $_POST["email"];
+$senha = $_POST["senha"];
+$celular = $_POST["celular"];
+$telefone = $_POST["telefone"];
+$turma = $_POST["turma"];
+
+$image_file = $_FILES["foto_file"]["name"];
+$type  = $_FILES["foto_file"]["type"]; //file name "foto_file" 
+$size  = $_FILES["foto_file"]["size"];
+$temp  = $_FILES["foto_file"]["tmp_name"];
+$error  = $_FILES["foto_file"]["error"];
+// print_r($imagem);exit;
+
+if (($rastro != "") && ($nome != "") && ($rg != "") && ($cpf != "") && ($nascimento != "") && ($email != "") && ($senha != "") && ($celular !="")) {
+
+	//limpando RA
 	$ra = str_replace('-', '', $rastro);
-
-	$nome = $_POST["nome"];
-	$rg = $_POST["rg"];
-	
-	$cpf = $_POST["cpf"];
+	//Limpando cpf aluno
 	$limcpf =  str_replace('.', '', $cpf);
 	$cpf_aluno =  str_replace('-', '', $limcpf);
+	//ajustando data de acordo com banco
+	$data_nascimento = date('Y/m/d', strtotime($nascimento));
 
-	$nascimento = $_POST["data_nascimento"];
-	$data_nascimento = date('Y/m/d',strtotime($nascimento));
-	$email = $_POST["email"];
-	$senha = $_POST["senha"];
-	$confsenha = $_POST["confsenha"];
-	$celular = $_POST["celular"];
-	$telefone = $_POST["telefone"];
-	$turma = $_POST["turma"];
-
-	
-	$image_file = $_FILES["foto_file"]["name"];
-	$type  = $_FILES["foto_file"]["type"]; //file name "foto_file" 
-	$size  = $_FILES["foto_file"]["size"];
-	$temp  = $_FILES["foto_file"]["tmp_name"];
-	$error  = $_FILES["foto_file"]["error"];
-	// print_r($imagem);exit;
-	
-	if ($error==1){
+	if ($error == 1) {
 		echo "<script>alert('O arquivo no upload é maior do que o limite definido em upload_max_filesize no php.ini');
 			    history.back();
-		    </script>";
-	 }
+				</script>";
+	}
 	//print_r($imagem);exit;
 	if ($image_file) {
 
 		$largura = 2000;
 		$altura = 3000;
 		$tamanho = 2000000;
-    	
-    	if(!preg_match("/\.(gif|bmp|png|jpg|jpeg){1}$/i", $image_file, $ext)){
-            echo "<script>alert('Ops! Isso não é uma imagem.');
-                     history.back();
+
+		if (!preg_match("/\.(gif|bmp|png|jpg|jpeg){1}$/i", $image_file, $ext)) {
+			echo "<script>alert('Ops! Isso não é uma imagem.');
+			history.back();
 				 </script>";
-            
-   	 	}
-	
+		}
+
 		$dimensoes = getimagesize($temp);
 		//print_r($dimensoes);exit;	
-		if($dimensoes[0] > $largura) {
-			echo "A largura da imagem não deve ultrapassar ".$largura." pixels";
+		if ($dimensoes[0] > $largura) {
+			echo "A largura da imagem não deve ultrapassar " . $largura . " pixels";
 		}
 
-		if($dimensoes[1] > $altura) {
-			echo "Altura da imagem não deve ultrapassar ".$altura." pixels";
+		if ($dimensoes[1] > $altura) {
+			echo "Altura da imagem não deve ultrapassar " . $altura . " pixels";
 		}
-		
-		if($size > $tamanho) {
-   		 	echo "A imagem deve ter no máximo ".$tamanho." bytes";
-		}else {
-			
+
+		if ($size > $tamanho) {
+			echo "A imagem deve ter no máximo " . $tamanho . " bytes";
+		} else {
+
 			preg_match("/\.(gif|bmp|png|jpg|jpeg){1}$/i", $image_file, $ext);
 
-        	$nome_imagem = md5(uniqid(time())) . "." . $ext[1];
-        	
-        	$caminho = "../../assets/imagensBanco/" . $nome_imagem;
-			
+			$nome_imagem = md5(uniqid(time())) . "." . $ext[1];
+
+			$caminho = "../../assets/imagensBanco/" . $nome_imagem;
+
 			move_uploaded_file($temp, $caminho);
-			
+
 			$query = $conn->prepare("INSERT INTO aluno (RA,nome_aluno, foto, RG, cpf, email, senha, celular, telefone, data_nascimento, fk_id_turma_aluno, fk_id_responsavel_aluno, fk_id_tipo_usuario_aluno, fk_id_escola_aluno)
             VALUES ('{$ra}','{$nome}','{$nome_imagem}','{$rg}','{$cpf_aluno}','{$email}','{$senha}','{$celular}','{$telefone}','{$data_nascimento}','{$turma}','{$id_responsavel}','5', '{$id_escola}')");
 
-            if ($query->execute()) {
+			if ($query->execute()) {
 
 				if ($id_tipo_usuario == 2) {
 
 					echo "<script>alert('Aluno cadastrado com sucesso');
-						window.location='../../homeDiretor.html.php';
-						</script>";
+					window.location='../../homeDiretor.html.php';
+					</script>";
 				} elseif ($id_tipo_usuario == 3) {
-	
+
 					echo "<script>alert('Aluno cadastrado com sucesso');
-						window.location='../../homeSecretaria.html.php';
-						</script>";
+					window.location='../../homeSecretaria.html.php';
+					</script>";
 				} else {
 					echo "<script>alert('Usuario sem permissão');
-					 window.location='../../index.php'</script>";
+					window.location='../../index.php'</script>";
 				}
-				 
-            }else{
+			} else {
 				echo "<script>alert('Erro: Aluno não foi cadastrado');
 				history.back();;
-				 </script>";
-				
-			
-
-				 
-            }
-
-		
+				</script>";
+			}
 		}
-    }
-  
-?>
+	}
+} else {
+	echo "<script>alert('Preencha os Campos');
+			history.back();</script>";
+}
--- php/cadastroContas/cadastrarAluno.php
+++ php/cadastroContas/cadastrarAluno.php
@@ -7,33 +7,16 @@ $id_escola = $_SESSION["id_escola"];
 $id_tipo_usuario = $_SESSION["id_tipo_usuario"];
 
 $rastro = $_POST["ra"];
-$ra = str_replace('-', '', $rastro);
-
 $nome = $_POST["nome"];
 $rg = $_POST["rg"];
-
 $cpf = $_POST["cpf"];
-$limcpf =  str_replace('.', '', $cpf);
-$cpf_aluno =  str_replace('-', '', $limcpf);
-
 $nascimento = $_POST["data_nascimento"];
-$data_nascimento = date('Y/m/d',strtotime($nascimento));
 $email = $_POST["email"];
 $senha = $_POST["senha"];
-$confsenha = $_POST["confsenha"];
 $celular = $_POST["celular"];
 $telefone = $_POST["telefone"];
 $turma = $_POST["turma"];
-
-
-// Resgatando responsavel
 $cpf_respon = $_POST["cpf_respon"];
-$cpf_res = str_replace('.', '', $cpf_respon);
-$cpf_responsavel = str_replace('-', '', $cpf_res);
-$query = $conn->prepare("select id_responsavel from responsavel where cpf=$cpf_responsavel");
-$query->execute();
-$dados = $query->fetch(PDO::FETCH_ASSOC);
-$id_responsavel = $dados['id_responsavel'];
 
 $image_file = $_FILES["foto_file"]["name"];
 $type  = $_FILES["foto_file"]["type"]; //file name "foto_file" 
@@ -42,68 +25,90 @@ $temp  = $_FILES["foto_file"]["tmp_name"];
 $error  = $_FILES["foto_file"]["error"];
 // print_r($imagem);exit;
 
-if ($error == 1) {
-	echo "<script>alert('O arquivo no upload é maior do que o limite definido em upload_max_filesize no php.ini');
+if (($rastro != "") && ($nome != "") && ($rg != "") && ($cpf != "") && ($nascimento != "") && ($email != "") && ($senha != "") && ($celular != "") && ($cpf_respon != "")) {
+
+	//limpando RA
+	$ra = str_replace('-', '', $rastro);
+	//limpando CPF aluno
+	$limcpf =  str_replace('.', '', $cpf);
+	$cpf_aluno =  str_replace('-', '', $limcpf);
+	//ajustando a data de acordo com o banco 
+	$data_nascimento = date('Y/m/d', strtotime($nascimento));
+	// Resgatando responsavel
+	$cpf_res = str_replace('.', '', $cpf_respon);
+	$cpf_responsavel = str_replace('-', '', $cpf_res);
+	$query = $conn->prepare("select id_responsavel from responsavel where cpf=$cpf_responsavel");
+	$query->execute();
+	$dados = $query->fetch(PDO::FETCH_ASSOC);
+	$id_responsavel = $dados['id_responsavel'];
+
+
+	if ($error == 1) {
+		echo "<script>alert('O arquivo no upload é maior do que o limite definido em upload_max_filesize no php.ini');
 			    history.back();
-		    </script>";
-}
-//print_r($imagem);exit;
-if ($image_file) {
-
-	$largura = 2000;
-	$altura = 3000;
-	$tamanho = 2000000;
-
-	if (!preg_match("/\.(gif|bmp|png|jpg|jpeg){1}$/i", $image_file, $ext)) {
-		echo "<script>alert('Ops! Isso não é uma imagem.');
-                     history.back();
-				 </script>";
+				</script>";
 	}
+	//print_r($imagem);exit;
+	if ($image_file) {
 
-	$dimensoes = getimagesize($temp);
-	//print_r($dimensoes);exit;	
-	if ($dimensoes[0] > $largura) {
-		echo "A largura da imagem não deve ultrapassar " . $largura . " pixels";
-	}
+		$largura = 2000;
+		$altura = 3000;
+		$tamanho = 2000000;
 
-	if ($dimensoes[1] > $altura) {
-		echo "Altura da imagem não deve ultrapassar " . $altura . " pixels";
-	}
+		if (!preg_match("/\.(gif|bmp|png|jpg|jpeg){1}$/i", $image_file, $ext)) {
+			echo "<script>alert('Ops! Isso não é uma imagem.');
+		history.back();
+		</script>";
+		}
 
-	if ($size > $tamanho) {
-		echo "A imagem deve ter no máximo " . $tamanho . " bytes";
-	} else {
+		$dimensoes = getimagesize($temp);
+		//print_r($dimensoes);exit;	
+		if ($dimensoes[0] > $largura) {
+			echo "A largura da imagem não deve ultrapassar " . $largura . " pixels";
+		}
 
-		preg_match("/\.(gif|bmp|png|jpg|jpeg){1}$/i", $image_file, $ext);
+		if ($dimensoes[1] > $altura) {
+			echo "Altura da imagem não deve ultrapassar " . $altura . " pixels";
+		}
 
-		$nome_imagem = md5(uniqid(time())) . "." . $ext[1];
+		if ($size > $tamanho) {
+			echo "A imagem deve ter no máximo " . $tamanho . " bytes";
+		} else {
 
-		$caminho = "../../assets/imagensBanco/" . $nome_imagem;
+			preg_match("/\.(gif|bmp|png|jpg|jpeg){1}$/i", $image_file, $ext);
 
-		move_uploaded_file($temp, $caminho);
+			$nome_imagem = md5(uniqid(time())) . "." . $ext[1];
 
-		$query = $conn->prepare("INSERT INTO aluno (RA,nome_aluno, foto, RG, cpf, email, senha, celular, telefone, data_nascimento, fk_id_turma_aluno, fk_id_responsavel_aluno, fk_id_tipo_usuario_aluno, fk_id_escola_aluno)
+			$caminho = "../../assets/imagensBanco/" . $nome_imagem;
+
+			move_uploaded_file($temp, $caminho);
+
+			$query = $conn->prepare("INSERT INTO aluno (RA,nome_aluno, foto, RG, cpf, email, senha, celular, telefone, data_nascimento, fk_id_turma_aluno, fk_id_responsavel_aluno, fk_id_tipo_usuario_aluno, fk_id_escola_aluno)
             VALUES ('{$ra}','{$nome}','{$nome_imagem}','{$rg}','{$cpf_aluno}','{$email}','{$senha}','{$celular}','{$telefone}','{$data_nascimento}','{$turma}','{$id_responsavel}','5', '{$id_escola}')");
 
-		if ($query->execute()) {
-			if ($id_tipo_usuario == 2) {
+			if ($query->execute()) {
+				if ($id_tipo_usuario == 2) {
 
-				echo "<script>alert('Aluno cadastrado com sucesso');
+					echo "<script>alert('Aluno cadastrado com sucesso');
 					window.location='../../homeDiretor.html.php';
 					</script>";
-			} elseif ($id_tipo_usuario == 3) {
+				} elseif ($id_tipo_usuario == 3) {
 
-				echo "<script>alert('Aluno cadastrado com sucesso');
+					echo "<script>alert('Aluno cadastrado com sucesso');
 					window.location='../../homeSecretaria.html.php';
 					</script>";
+				} else {
+					echo "<script>alert('Usuario sem permissão');
+					window.location='../../index.php'</script>";
+				}
 			} else {
-				echo "<script>alert('Usuario sem permissão');
-         		window.location='../../index.php'</script>";
-			}
-		} else {
-			echo "<script>alert('Erro: Aluno não foi cadastrado');
+				echo "<script>alert('Erro: Aluno não foi cadastrado');
 				history.back();;
-				 </script>";
+				</script>";
+			}
 		}
 	}
+} else {
+	echo "<script>alert('Preencha os Campos');
+	history.back();</script>";
 }
