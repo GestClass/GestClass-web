5b9ddd41e0c98d0a5613425d0468ea25c86c343f
Caio Fonseca <fcaio8137@gmail.com>
2020-06-18 03:11:44 -0300

    Caio: Validação para não enviar documentos vazios

--- php/enviarMensalidades/enviarMensalidade.php
+++ php/enviarMensalidades/enviarMensalidade.php
@@ -11,47 +11,52 @@ $id_responsavel = $_SESSION["id_respon"];
 
 $boleto = $_FILES["boleto"]['name'];
 move_uploaded_file($_FILES["boleto"]["tmp_name"], "../../assets/boletos/" . $boleto);
+if ($boleto != "") {
 
-if ($id_tipo_usuario == 2) {
+    if ($id_tipo_usuario == 2) {
 
-    $query_envio = $conn->prepare("INSERT INTO envio_boleto (fk_id_diretor_envio_boleto, fk_id_secretario_envio_boleto, fk_id_responsavel_recebimento_boleto, data_envio, boleto) 
+        $query_envio = $conn->prepare("INSERT INTO envio_boleto (fk_id_diretor_envio_boleto, fk_id_secretario_envio_boleto, fk_id_responsavel_recebimento_boleto, data_envio, boleto) 
     VALUES (:id_usuario, NULL, :id_responsavel, NOW(), :boleto)");
 
-    $query_envio->bindParam(':id_usuario', $id_usuario, PDO::PARAM_INT);
-    $query_envio->bindParam(':id_responsavel', $id_responsavel, PDO::PARAM_INT);
-    $query_envio->bindParam(':boleto', $boleto, PDO::PARAM_STR);
+        $query_envio->bindParam(':id_usuario', $id_usuario, PDO::PARAM_INT);
+        $query_envio->bindParam(':id_responsavel', $id_responsavel, PDO::PARAM_INT);
+        $query_envio->bindParam(':boleto', $boleto, PDO::PARAM_STR);
 
-    $query_envio->execute();
+        $query_envio->execute();
 
-    if ($query_envio->rowCount()) {
-        echo "<script>alert('Mensalidade enviada com sucesso');
+        if ($query_envio->rowCount()) {
+            echo "<script>alert('Mensalidade enviada com sucesso');
         window.location='../../homeDiretor.html.php';
         </script>";
-    } else {
+        } else {
 
-        echo "<script>alert('Erro: Mensalidade não enviada');
+            echo "<script>alert('Erro: Mensalidade não enviada');
     history.back();;
     </script>";
-    }
-} else {
+        }
+    } else {
 
-    $query_envio = $conn->prepare("INSERT INTO envio_boleto (fk_id_diretor_envio_boleto, fk_id_secretario_envio_boleto, fk_id_responsavel_recebimento_boleto, data_envio, boleto) 
+        $query_envio = $conn->prepare("INSERT INTO envio_boleto (fk_id_diretor_envio_boleto, fk_id_secretario_envio_boleto, fk_id_responsavel_recebimento_boleto, data_envio, boleto) 
     VALUES (NULL, :id_usuario, :id_responsavel, NOW(), :boleto)");
 
-    $query_envio->bindParam(':id_usuario', $id_usuario, PDO::PARAM_INT);
-    $query_envio->bindParam(':id_responsavel', $id_responsavel, PDO::PARAM_INT);
-    $query_envio->bindParam(':boleto', $boleto, PDO::PARAM_STR);
+        $query_envio->bindParam(':id_usuario', $id_usuario, PDO::PARAM_INT);
+        $query_envio->bindParam(':id_responsavel', $id_responsavel, PDO::PARAM_INT);
+        $query_envio->bindParam(':boleto', $boleto, PDO::PARAM_STR);
 
-    $query_envio->execute();
+        $query_envio->execute();
 
-    if ($query_envio->rowCount()) {
-        echo "<script>alert('Mensalidade enviada com sucesso');
+        if ($query_envio->rowCount()) {
+            echo "<script>alert('Mensalidade enviada com sucesso');
         window.location='../../homeSecretaria.html.php';
         </script>";
-    } else {
+        } else {
 
-        echo "<script>alert('Erro: Mensalidade não enviada');
+            echo "<script>alert('Erro: Mensalidade não enviada');
     history.back();;
     </script>";
+        }
     }
+} else {
+    echo "<script>alert('Escolha um arquivo para enviar')
+    history.back()</script>";
 }
