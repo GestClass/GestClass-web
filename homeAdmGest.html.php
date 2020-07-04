d5e51ea65980538af12d33c7f7f770e51fc67d77
Eric Veludo <eric.oliveira46@etec.sp.gov.br>
2020-07-04 16:30:38 -0300

    Eric: alterações na home adm

--- homeAdmGest.html.php
+++ homeAdmGest.html.php
@@ -113,11 +113,11 @@
             <ul>
 
                 <!-- <li><a href="feedbackEscolas.html.php" class="btn-floating blue-grey tooltipped" data-position="left" data-tooltip="Feedback dos Usuários"><i class="material-icons">email</i></a></li> -->
-                <li><a href="calendario.html.php" class="btn-floating blue tooltipped" data-position="left" data-tooltip="Visão Geral"><i class="material-icons">explore</i></a></li>
+                <li><a href="solicitacoesadm.html.php" class="btn-floating tooltipped" data-position="left" data-tooltip="Admins"><i class="material-icons">add_alert</i></a></li>
                 <li><a href="feedbackEscolas.html.php" class="btn-floating blue-grey tooltipped" data-position="left" data-tooltip="Feedback dos Usuários"><i class="material-icons">email</i></a></li>
                 <li><a href="visaoGeral.html.php" class="btn-floating blue tooltipped" data-position="left" data-tooltip="Visão Geral"><i class="material-icons">explore</i></a></li>
                 <li><a href="cadastroEscola.html.php" class="btn-floating black tooltipped" data-position="left" data-tooltip="Cadastro Escolas"><i class="material-icons">account_balance</i></a></li>
-                <li><a href="admins.html.php" class="btn-floating red tooltipped" data-position="left" data-tooltip="Admins"><i class="material-icons">perm_identity</i></a></li>
+                <li><a href="admins.html.php" class="btn-floating red tooltipped" data-position="left" data-tooltip="Admins"><i class="material-icons">perm_identity</i></a></li>                
             </ul>
         </div>
     </section>
