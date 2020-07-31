774f4f98453d447449003f74950ce5419fa2af3d
Caio Fonseca <fcaio8137@gmail.com>
2020-07-31 00:46:55 -0300

    Caio - ajustes no pin

--- php/pinNotif.php
+++ php/pinNotif.php
@@ -1,6 +1,6 @@
 <?php
     include_once 'conexao.php';
-    $pin = $_POST['pin'];
+    $pin = md5($_POST['pin']);
     $dados["id"]=$_SESSION["id_usuario"];
     $query = $conn->prepare("select ID_responsavel,pin from responsavel where ID_responsavel=:ID_responsavel");
     $query->bindValue(":ID_responsavel",$dados["id"]);
@@ -10,7 +10,7 @@
     if($pin == ""){
         echo "<script>alert('Digite seu PIN de segurança, por favor:)');
         window.location='../homePais.html.php'</script>";
-    }else if($pin==md5($dados['pin'])){
+    }else if($pin==$dados['pin']){
         echo "<script>alert('Validação efetuada com sucesso;)');
         window.location='../mensagensResponsavel.html.php'
         </script>";
