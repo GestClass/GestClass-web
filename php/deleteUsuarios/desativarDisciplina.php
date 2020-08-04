3eae4b2106f56a29510460e206f509db9c710fc2
Eric Veludo <eric.oliveira46@etec.sp.gov.br>
2020-08-03 22:02:00 -0300

    Eric: alteração no bd

--- php/deleteUsuarios/desativarDisciplina.php
+++ php/deleteUsuarios/desativarDisciplina.php
@@ -10,26 +10,39 @@ $id_escola = $_SESSION["id_escola"];
 $id_disciplina = $_GET['idDisciplina'];
 $situacao = false;
 
-$sql_update_desativar = $conn->prepare("UPDATE disciplina SET situacao = :false WHERE fk_id_escola_disciplina = :id_escola AND ID_disciplina = :id_disciplina");
-$sql_update_desativar->bindParam(':false', $situacao, PDO::FETCH_ASSOC);
-$sql_update_desativar->bindParam(':id_escola', $id_escola, PDO::FETCH_ASSOC);
-$sql_update_desativar->bindParam(':id_disciplina', $id_disciplina, PDO::FETCH_ASSOC);
-$sql_update_desativar->execute();
+// Verificar existência de grade curricular com essa disciplina
+$sql_select_grade = $conn->prepare("SELECT turma.nome_turma AS turma FROM grade_curricular INNER JOIN turma ON (grade_curricular.fk_id_turma_grade_curricular = turma.ID_turma) WHERE grade_curricular.fk_id_disciplina_grade_curricular = $id_disciplina GROUP BY grade_curricular.fk_id_turma_grade_curricular;");
+$sql_select_grade->execute();
 
-if ($sql_update_desativar->rowCount()) {
+if (!$sql_select_grade->rowCount()) {
+
+    $sql_update_desativar = $conn->prepare("UPDATE disciplina SET situacao = :false WHERE fk_id_escola_disciplina = :id_escola AND ID_disciplina = :id_disciplina");
+    $sql_update_desativar->bindParam(':false', $situacao, PDO::FETCH_ASSOC);
+    $sql_update_desativar->bindParam(':id_escola', $id_escola, PDO::FETCH_ASSOC);
+    $sql_update_desativar->bindParam(':id_disciplina', $id_disciplina, PDO::FETCH_ASSOC);
+    $sql_update_desativar->execute();
+
+    if ($sql_update_desativar->rowCount()) {
 ?>
-    <script>
-        alert('Disciplina desativada com sucesso!');
-        window.location = '../../listaDisciplinas.html.php';
-    </script>
+        <script>
+            alert('Disciplina desativada com sucesso!');
+            window.location = '../../listaDisciplinas.html.php';
+        </script>
+    <?php
+    } else {
+    ?>
+        <script>
+            alert('Erro ao desativas disciplina!');
+            window.location = '../../listaDisciplinas.html.php';
+        </script>
 <?php
+    }
 } else {
-?>
+    ?>
     <script>
-        alert('Erro ao desativas disciplina!');
+        alert('Erro ao desativas disciplina, você possui grade(s) curricular(es) com esta disciplina cadastrada!\n\nTurma(s):<?php while($array_turmas = $sql_select_grade->fetch(PDO::FETCH_ASSOC)) { $nome_turma =  $array_turmas['turma']; echo '   ' . $nome_turma . ';   '; }?>');
         window.location = '../../listaDisciplinas.html.php';
     </script>
 <?php
 }
-
 ?>
